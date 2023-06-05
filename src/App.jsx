521676c4a7da40a98433f3c470817fe9712dead5
tbrown034 <tbrown034@gmail.com>
2023-06-05 16:28:01 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <section className=" bg-indigo-600 text-white flex flex-col justify-center items-center min-h-screen gap-8">
+        <h1 className="text-6xl">Dental GPT</h1>
+        <form>
+          <textarea className=" text-slate-950   max-w-max"></textarea>
+        </form>
+      </section>
+    </>
+  );
+}
+
+export default App;
